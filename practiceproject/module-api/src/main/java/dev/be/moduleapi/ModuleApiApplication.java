0f79ede56fafba8dfe70f8ed2becee6d81cacc97
mrcocoball <gunmaru93@gmail.com>
2023-03-27 09:04:36 +0900

    0326

--- practiceproject/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ practiceproject/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -2,10 +2,14 @@ package dev.be.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
-        scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"}
+        scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"} // 컴포넌트 스캔 대상 패키지 지정
 )
+@EntityScan("dev.be.modulecommon.domain") // 엔티티 패키지 지정
+@EnableJpaRepositories(basePackages = "dev.be.modulecommon.repositories") // 리포지토리 패키지 지정
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
